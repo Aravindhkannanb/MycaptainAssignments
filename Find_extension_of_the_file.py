756a121f7e378a1a8a56cb0b68165730b65ed406
Aravindhkannanb <99198670+Aravindhkannanb@users.noreply.github.com>
2022-02-08 21:11:32 +0530

    Delete Find_extension_of_the_file.py

--- Find_extension_of_the_file.py
+++
@@ -1,9 +0,0 @@
-x = input("Input the file name : ")
-v = x.split(".")
-b = repr(v[1])
-
-print("The extension of the file {} is {}".format(x,b))
-if(b[1:len(b)-1]=='py'):
-    print("The Extension of the file is : Python")
-else:
-    print("bye")
