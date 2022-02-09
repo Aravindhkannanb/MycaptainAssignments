8a653140527a50a7f09db0618e4807fc6c157e39
Aravindhkannanb <99198670+Aravindhkannanb@users.noreply.github.com>
2022-02-09 21:06:42 +0530

    Add files via upload

---
+++ Fibonacci series.py
@@ -0,0 +1,17 @@
+def aravindh(n):
+    x = 0
+    y =1
+    print (x)
+    print(y)
+    i=2
+    while True:
+
+        z = x+y
+        print(z)
+        x = y 
+        y = z
+        i= i+1
+
+
+
+aravindh(10000)
